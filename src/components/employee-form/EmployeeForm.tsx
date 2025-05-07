3dabc763100c9094afc54940e114888113e8e0e1
Yuragranovsky <yuragranovsky@gmail.com>
2025-05-07 20:55:23 +0300

    CW #34

--- src/components/employee-form/EmployeeForm.tsx
+++ src/components/employee-form/EmployeeForm.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, {  useState } from 'react'
 import Employee from '../../model/Employee';
 import './EmployeeForm.css'
 import { useForm } from 'react-hook-form';
@@ -11,36 +11,47 @@ const EmployeeForm: React.FC<Props> = ({submitter}) => {
 //    const formElem = document.querySelector("form") as HTMLFormElement; it's unaccepted
  //  const formRef = useRef<HTMLFormElement>(null);
 
-  return <form  onSubmit={handleSubmit(data => {
+  return <form  className="ms-5"onSubmit={handleSubmit(data => {
      submitter(data)
      reset()
      })} onReset={() => setIsDevelopment(false)}>
-       <input {...register("name",{required:true, minLength: 3, maxLength: 10})} type='text' placeholder='Enter name'/>
-       { formState.errors?.name?.type === 'required' && <p className="error">Field name must be filled</p>}
-       { formState.errors?.name?.type === 'minLength' && <p className="error">name must have at least 3 symbols</p>}
-       <input type="number" {...register("salary", {required:true, min: 5000, max: 50000})} placeholder='Enter salary'/>
+       <div className=" form-floating mb-3">
+         
+          <input id="name" className="form-control" {...register("name",{required:true, minLength: 3, maxLength: 10})} type='text' placeholder='Enter name'/>
+          <label htmlFor="name" >Enter Name</label>
+          </div>
+       { formState.errors?.name?.type === 'required' && <p className="error form-text">Field name must be filled</p>}
+       { formState.errors?.name?.type === 'minLength' && <p className="error form-text">name must have at least 3 symbols</p>}
+       <div className="form-floating mb-3">
+          
+          <input id="salary" className="form-control" type="number" {...register("salary", {required:true, min: 5000, max: 50000})} placeholder='Enter salary'/>
+          <label htmlFor="" > Enter Salary</label>
+          </div>
        { formState.errors?.salary?.type === 'required' && <p className="error">salary should be entered</p>}
-       { formState.errors?.salary?.type === 'min' && <p className="error">minimal salary is 5000</p>}
-       { formState.errors?.salary?.type === 'max' && <p className="error">maximal salary is 50000</p>}
-       <select {...register("department", {required:true, onChange: (event) => {
-          setIsDevelopment(event.target.value === "Development")
-      }})}>
+       { formState.errors?.salary?.type === 'min' && <p className="error form-text">minimal salary is 5000</p>}
+       { formState.errors?.salary?.type === 'max' && <p className="error form-text">maximal salary is 50000</p>}
+       <div className="mb-3">
+            <select className="form-select" {...register("department", {required:true, onChange: (event) => {
+               setIsDevelopment(event.target.value === "Development")
+                  }})}>
+       
             <option value="">--Select Department--</option>
             <option value="Development">Development</option>
             <option value="QA">QA</option>
             <option value="Management">Management</option>
        </select>
-       { formState.errors?.department?.type === 'required' && <p className="error">department should be selected</p>}
-      {isDevelopment && <select {...register("languages")} multiple>
+       </div>
+       { formState.errors?.department?.type === 'required' && <p className="error form-text">department should be selected</p>}
+      {isDevelopment && <select className="mb-3 form-select" {...register("languages")} multiple>
           <option value="">--Select Languages--</option>
           <option value="C++">C++</option>
           <option value="JavaScript">JavaScript</option>
           <option value="Java">Java</option>
           <option value="Python">Python</option>
        </select>}
-       <div>
-         <button type="submit">Submit</button>
-         <button type="reset">Reset</button>
+       <div className="mb-3 d-flex justify-content-around">
+         <button type="submit" className="btn btn-primary">Submit</button>
+         <button type="reset"  className="btn btn-primary">Reset</button>
        </div>
   </form>
 }
